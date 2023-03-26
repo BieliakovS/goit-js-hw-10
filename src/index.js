1f1a81da69d6eb2382b69249a1a1b956ae7deba4
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-03-27 02:24:31 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries';
+import CountryList from './templates/countryList.hbs';
+import CountryCard from './templates/countryCard.hbs';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchBox: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+function onInput(e) {
+  e.preventDefault();
+  const inputValue = refs.searchBox.value.trim();
+
+  fetchCountries(inputValue).then(country => {
+    countryList.innerHTML = CountryList(country);
+  });
+
+  // if (inputValue === '') {
+  //     console.log('Enter country name');
+  // } if (fetchCountries(inputValue).length <= 10) {
+  //     console.log('list')
+  // }
+  // fetchCountries(inputValue).then(country => {
+  //     countryList.innerHTML = CountryList(country);
+  // });
+}
+
+refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
