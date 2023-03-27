58a639f563b84652d02e7c075b9eac3b669df3f8
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-03-27 22:03:58 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './js/fetchCountries';
 import CountryListTemplate from './templates/countryList.hbs';
 import CountryCardTemplate from './templates/countryCard.hbs';
 
 const DEBOUNCE_DELAY = 300;
-
-
 
 const refs = {
   searchBox: document.querySelector('#search-box'),
@@ -18,25 +17,32 @@ function onInput(e) {
   e.preventDefault();
   const inputValue = refs.searchBox.value.trim();
 
-fetchCountries(inputValue)
-.then(value => {
-  if (value.length === 0) {
-    console.log('Please, enter country name');
-    return 'Please, enter country name';
-  } if (value.length > 10) {
-    console.log('Too many matches found. Please enter a more specific name.');
-  } if (value.length >= 2 && value.length <= 10) {
-    console.log(refs.countryList);
-    refs.countryList.innerHTML = CountryListTemplate(value[0]);
-  } if (value.length === 1) {
-    console.log(value[0]);
-    refs.countryInfo.innerHTML = CountryCardTemplate(value);
+  if (inputValue === '') {
+    Notiflix.Notify.warning('Please, enter country name');
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
   }
-})
-.catch(error => {
-  console.warn(error);
-});
+
+  fetchCountries(inputValue)
+    .then(value => {
+      if (value.length > 10) {
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      }
+      if (value.length >= 2 && value.length <= 10) {
+        console.log(refs.countryList);
+        refs.countryList.innerHTML = CountryListTemplate(value[0]);
+      }
+      if (value.length === 1) {
+        console.log(value[0]);
+        refs.countryInfo.innerHTML = CountryCardTemplate(value);
+      }
+    })
+    .catch(error => {
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
 
 refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
-
