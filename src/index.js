fb2a31c0e8d35f14ee1e88fc895202cf398672bd
BieliakovS <93442308+BieliakovS@users.noreply.github.com>
2023-03-27 21:32:42 +0300

    test commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import './css/styles.css';
-import fetchCountries from './js/fetchCountries';
-// import CountryList from './templates/countryList';
-// import CountryCard from './templates/countryCard';
 import debounce from 'lodash.debounce';
+import { fetchCountries } from './js/fetchCountries';
+import CountryListTemplate from './templates/countryList.hbs';
+import CountryCardTemplate from './templates/countryCard.hbs';
 
 const DEBOUNCE_DELAY = 300;
+
+
 
 const refs = {
   searchBox: document.querySelector('#search-box'),
@@ -16,18 +18,25 @@ function onInput(e) {
   e.preventDefault();
   const inputValue = refs.searchBox.value.trim();
 
-  // fetchCountries(inputValue).then(country => {
-  //   countryList.innerHTML = CountryList(country);
-  // });
-
-  // if (inputValue === '') {
-  //     console.log('Enter country name');
-  // } if (fetchCountries(inputValue).length <= 10) {
-  //     console.log('list')
-  // }
-  // fetchCountries(inputValue).then(country => {
-  //     countryList.innerHTML = CountryList(country);
-  // });
+fetchCountries(inputValue)
+.then(value => {
+  if (value.length === 0) {
+    console.log('Please, enter country name');
+    return 'Please, enter country name';
+  } if (value.length > 10) {
+    console.log('Too many matches found. Please enter a more specific name.');
+  } if (value.length >= 2 && value.length <= 10) {
+    console.log(refs.countryList);
+    refs.countryList.innerHTML = CountryListTemplate(value[0]);
+  } if (value.length === 1) {
+    console.log(value[0]);
+    refs.countryInfo.innerHTML = CountryCardTemplate(value);
+  }
+})
+.catch(error => {
+  console.warn(error);
+});
 }
 
 refs.searchBox.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,16 +1,16 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
 
-export const fetchCountries = value => {
+export const fetchCountries = value =>
   fetch(
     `${BASE_URL}/name/${value}?fields=name,capital,population,flags,languages`
   )
     .then(response => {
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error(`HTTP error! status: ${response.status}`);
       }
       return response.json();
     })
     .catch(error => {
       console.warn(error);
+      return null;
     });
-}
